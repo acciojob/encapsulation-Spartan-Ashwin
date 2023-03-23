ae22ea28caa36ddcd90e6475e34ce6726ff8001f
Ashwin Shelke <ashwinkumarshelke9@gmail.com>
2023-03-23 12:16:21 +0530

    Encapsulation Assignment

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) {
+        RWOnly obj=new RWOnly();
+        Scanner sc=new Scanner(System.in);
+        obj.setName(sc.next());
+        System.out.println(obj.getName());
+    }
   
 }
